83860786b3205d1902d0f679c3eff4fa0fe97deb
Sanj Singh <sanjaysinghmuchhal@gmail.com>
2023-01-25 12:41:12 +0530

    added two dir

---
+++ src/createProduct/createProduct.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function createProduct() {
+  return (
+    <div className="sample">
+    </div>
+  );
+}
+
+export default createProduct;
---
+++ src/getProductDetails/getProductDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function getProductDetails() {
+  return (
+    <div className="sample">
+    </div>
+  );
+}
+
+export default getProductDetails;
