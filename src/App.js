dfecef5f2736a7b5dea539c5b973fd71e0f53ce6
SaurabhMsys <saurabh.singh@msystechnologies.com>
2023-02-07 11:10:54 +0530

    done createProduct

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import CreateProduct from './createProduct/createProduct';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CreateProduct/>
+  
+    </div>
+  );
+}
+
+export default App
--- src/createProduct/createProduct.tsx
+++ src/createProduct/createProduct.tsx
@@ -1,10 +1,104 @@
 import React from 'react';
+import {useState} from 'react'
+import './createProduct.css'
+// import { map } from 'rxjs/operators'
 
-function createProduct() {
+
+function CreateProduct() {
+  const [formVal, setFormVal]= useState({name: '', email: ''})
+  const onSubmit = () => {}
+
+
+
   return (
     <div className="sample">
+      <form onSubmit={onSubmit}>
+      
+        <div className='div1'>
+        <label className=' label-balance' >barcode</label>
+        <input  type= "text" name="name" value={""}/>
+        
+
+        <label className=' label-balance'>batchQuantity</label>
+        <input type= "text" name="name" value={""}/>
+
+        <br/>
+        
+        <label className=' label-balance'>category</label>
+        <input type= "text" name="name" value={""}/>
+
+        <label className=' label-balance'>componentProductIds</label>
+        <input type= "text" name="name" value={""}/>
+
+        <br/>
+
+        <label className=' label-balance'>expirationDate</label>
+        <input type= "text" name="name" value={""}/>
+
+        <label className=' label-balance'>id</label>
+        <input type= "text" name="name" value={""}/>
+
+        <br/>
+
+        <label className=' label-balance'>locationData</label>
+        <input type= "text" name="name" value={""}/>
+
+        <label className=' label-balance'>misc</label>
+        <input type= "text" name="name" value={""}/>
+
+        <br/>
+
+        <label className=' label-balance'>name</label>
+        <input type= "text" name="name" value={""}/>
+
+        <label className=' label-balance'>placeOfOrigin</label>
+        <input type= "text" name="name" value={""}/>
+
+        <br/>
+
+        <label className=' label-balance'>productionDate</label>
+        <input type= "text" name="name" value={""}/>
+
+        <label className=' label-balance'>unitPrice</label>
+        <input type= "text" name="name" value={""}/>
+
+        <br/>
+
+        <label className=' label-balance'>unitQuantity</label>
+        <input type= "text" name="name" value={""}/>
+
+        <label className=' label-balance'>unitQuantityType</label>
+        <input type= "text" name="name" value={""}/>
+
+        <br/>
+
+        <label  className=' label-balance'>variety</label>
+        <input type= "text" name="name" value={""}/>
+
+        <label className=' label-balance'>componentProducts</label>
+        <input type= "text" name="name" value={""}/>
+
+
+        <br/>
+
+        <label className=' label-balance'>Name</label>
+        <input type= "text" name="name" value={""}/>
+
+        <label className=' label-balance'>Email</label>
+        <input type= "text" name="name" value={""}/>
+        <br/>
+
+        <button className='button-style'>Submit</button>
+
+
+        </div>
+
+    
+
+</form>
+   
     </div>
   );
 }
 
-export default createProduct;
+export default CreateProduct;
